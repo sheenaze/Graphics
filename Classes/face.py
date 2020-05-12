189b5dd91ad358d5043e30520ceaaeedec0899e8
Monika Tercjak <monika.tercjak@gmail.com>
2020-05-12 14:29:19 +0200

    Class Face added so that I can print faces now

---
+++ Classes/face.py
@@ -0,0 +1,152 @@
+import matplotlib.patches as pt
+import matplotlib.pyplot as plt
+
+"""
+I am using matplotlib.patches library here, more info can be found on the website: https://matplotlib.org/api/patches_api.html
+"""
+
+
+class Face:
+    def __init__(self, radius, facecolor='yellow',
+                 edgecolor='black', linewidth=2):
+
+        self.radius = radius
+        self.facecolor = facecolor
+        self.edgecolor = edgecolor
+        self.lw = linewidth
+
+    def get_head(self, x, y):
+        """
+
+        Parameters
+        ----------
+        x : Float
+            x coordinate of the head's center.
+        y : Float
+            y coordinate of the head's center.
+
+        Returns
+        -------
+        head : matplotlib.patch Circle object
+
+
+        """
+        head = pt.Circle([x, y], self.radius,
+                         facecolor=self.facecolor,
+                         edgecolor=self.edgecolor,
+                         lw=self.lw)
+        return head
+
+    def get_eye(self, x, y, radius, facecolor, edgecolor):
+        """
+
+        Parameters
+        ----------
+        x : Float
+            x coordinate of the eye's center.
+        y : Float
+            y coordinate of the eye's center.
+        radius : Float
+            radius of an eye
+        facecolor : None or color
+            facecolor of an eye
+        edgecolor : None or color
+            edgececolor of an eye
+
+        Returns
+        -------
+        eye : matplotlib.patch Circle object
+
+        """
+
+        eye = pt.Circle([x, y], radius=radius,
+                        facecolor=facecolor, edgecolor=edgecolor)
+        return eye
+
+    def get_mouth(self, x, y, width, height, linewidth, facecolor,
+                  edgecolor, angle=0.0, theta1=0, theta2=360):
+        """
+
+
+        Parameters
+        ----------
+        x : Float
+            x coordinate of the mouth's center.
+        y : Float
+            y coordinate of the mouth's center.
+        width : Float
+            The length of the horizontal (x) axis.
+        height : Float
+            The length of the vertical (y) axis.
+        linewidth : Float
+            Width of the mouth line.
+        facecolor : None or color
+            Facecolor of the mouth line.
+        edgecolor : None or color
+            Edgecolor of the mouth line.
+        angle : Float, optional
+            Rotation of the ellipse in degrees (counterclockwise). The default is 0.0.
+        theta1 : Float, optional
+            Starting  angle of the arc in degrees. The default is 0.
+        theta2 : Float, optional
+            Ending angle of the arc in degrees.. The default is 360.
+
+        Returns
+        -------
+        mouth : matplotlib.patch Arc object
+
+        """
+
+        mouth = pt.Arc([x, y], width, height, angle=angle,
+                       theta1=theta1, theta2=theta2, lw=linewidth,
+                       facecolor=facecolor, edgecolor=edgecolor)
+
+        return mouth
+
+    def smiling_face(self, x=0, y=0, eye_facecolor='black',
+                     eye_edge_color=None, mouth_facecolor='black',
+                     mouth_edgecolor=None):
+        """
+
+        Parameters
+        ----------
+        x : Float, optional
+            x coordinate of the head's center.. The default is 0.
+        y : Float, optional
+            y coordinate of the head's center.. The default is 0.
+        eye_facecolor : None or color, optional
+            Eyes facecolor. The default is 'black'.
+        eye_edge_color : None or color, optional
+             Eyes edgeecolor. The default is None.
+        mouth_facecolor : None or color, optional
+            Mouth facecolor. The default is 'black'.
+        mouth_edgecolor : None or color, optional
+            Mouth edgecolor. The default is None.
+
+        Returns
+        -------
+        list of matplotlib.patches objects
+
+        """
+        head = self.get_head(x, y)
+
+        eye_x = self.radius * 0.35
+        eye_y = self.radius * 0.35
+        eye_radius = self.radius * 0.2
+
+        left_eye = self.get_eye(-eye_x, eye_y, eye_radius,
+                                eye_facecolor, eye_edge_color)
+        right_eye = self.get_eye(eye_x, eye_y, eye_radius,
+                                 eye_facecolor, eye_edge_color)
+
+        smile_x = x
+        smile_y = y - self.radius * 0.4
+
+        smile_width = self.radius * 1.2
+        smile_height = self.radius * 0.7
+
+        smile = self.get_mouth(smile_x, smile_y, smile_width,
+                               smile_height, 10, mouth_facecolor,
+                               mouth_edgecolor, theta1=190, theta2=350)
+
+        return [head, left_eye, right_eye, smile]
